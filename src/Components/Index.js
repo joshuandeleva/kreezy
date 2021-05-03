99ca7ebdf9a72e4195091d3e1ddd7918cb25f353
joshuandeleva <joshuandeleva90@gmail.com>
2021-05-03 16:09:15 +0300

    abt-secton

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Resume from "../assets/images/Resume.docx";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 import Navbar from "./Navbar";
 import Chrispic from "../assets/images/chrispic1.png";
 import { Grid, Container, Button, Link } from "@material-ui/core";
@@ -17,7 +18,7 @@ function Index() {
 								<h1>I'm Christopher Wanjohi</h1>
 								<p>
 									A Datascientist 🥑 ,Data engineer 👨‍💻and an
-									NLP practioneer 
+									NLP practioneer
 								</p>
 								<div className="nav-btns">
 									<Button class="contact">
@@ -37,6 +38,43 @@ function Index() {
 							</div>
 						</Grid>
 					</Grid>
+					<div className="about__me">
+						<Grid container>
+							<Grid item xl={3} md={6} lg={6} sm={6}>
+								<div className="aboutMe__left">
+									<img src={Chrispic} alt="" />
+								</div>
+							</Grid>
+							<Grid item xl={9} md={6} lg={6} sm={6}>
+								<div className="aboutMe__right">
+									<h1>About Me 👨‍💻</h1>
+									<p>
+										Hello, i'm experienced junoir data
+										scientist extracting insights from real
+										world data to derive meaningfull
+										intutions to help business solve
+										problems.Am proficient in
+										python,sql,leveraging on Machine
+										Learning, DeeepLearning
+									</p>
+									<div className="about-me-social-media">
+										<a href="https://www.facebook.com/wanjohi.kreezy/">
+											<FaFacebookF />
+										</a>
+										<a href="https://www.twitter.com/IamWanjohichris">
+											<FaTwitter />
+										</a>
+										<a href="https://www.linkedln.com/in/Wanjohi-christopher-b9bb9b196">
+											<FaLinkedinIn />
+										</a>
+										<a href="https://github.com/WanjohiChristopher">
+											<FaGithub />
+										</a>
+									</div>
+								</div>
+							</Grid>
+						</Grid>
+					</div>
 				</Container>
 			</div>
 		</div>
