2f890a26b8a2424b2d100ea92efac9df8b980c65
joshuandeleva <joshuandeleva90@gmail.com>
2021-07-04 21:28:38 +0300

    added routinng constants

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./About.css";
+function About() {
+	return (
+		<div className="aboutMe__page">
+			<h1>About Me</h1>
+		</div>
+	);
+}
+export default About;
---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export const HOME = "/";
+export const ABOUT = "/about";
+export const CONTACT = "/about";
+export const BLOG = "/blog";
+export const SERVICES = "/services";
