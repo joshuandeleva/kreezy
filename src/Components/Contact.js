dd668bf9ba1818c07f30186b9f35c48b3519e973
joshuandeleva <joshuandeleva90@gmail.com>
2021-07-05 19:02:13 +0300

    added contacts page refcator

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Contact.css";
 import Fade from "react-reveal/Fade";
+import Emails from "../assets/images/Emails.svg"
+import { IoIosPin } from "react-icons/io"
+import { BsPhone } from "react-icons/bs"
+import {HiMail} from "react-icons/hi"
 import { Container, Grid, Button } from "@material-ui/core";
 function Contact() {
 	return (
@@ -49,7 +53,25 @@ function Contact() {
 								</div>
 							</div>
 						</Grid>
-						<Grid item xl={6} sm={12} md={6} lg={6}></Grid>
+						<Grid item xl={6} sm={12} md={6} lg={6}>
+							<div className="contacts-me-right">
+								<img src={Emails} alt="" />
+								<div className="more-details">
+									<div className="more-details-1">
+										<IoIosPin />
+										<p>Nairobi , Kenya</p>
+									</div>
+									<div className="more-details-1">
+										<BsPhone />
+										<p>+254707926812</p>
+									</div>
+									<div className="more-details-1">
+										<HiMail />
+										<p>nyagachris411@gmail.com</p>
+									</div>
+								</div>
+							</div>
+						</Grid>
 					</Grid>
 				</Container>
 			</div>
