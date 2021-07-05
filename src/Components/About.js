bba89964ba522b9b52b5eb6ef08583f3059d4e78
joshuandeleva <joshuandeleva90@gmail.com>
2021-07-05 17:38:43 +0300

    added the skills in abt page

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,8 +5,23 @@ import Chrispic from "../assets/images/chrispic1.png";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { Grid, Container } from "@material-ui/core";
-import { GiAchievement } from "react-icons/gi";
-import { ImFileText } from "react-icons/im";
+import { GiAchievement, GiCheckMark } from "react-icons/gi";
+import { IoLogoTableau } from "react-icons/io5";
+import { FaHtml5, FaPython } from "react-icons/fa";
+import {
+	SiAmazonaws,
+	SiPostgresql,
+	SiMysql,
+	SiHeroku,
+	SiFlask,
+	SiJupyter,
+	SiVisualstudio,
+	SiPowerbi,
+	SiMicrosoftazure,
+	SiApachespark,
+} from "react-icons/si";
+import { ImFileText, ImGit } from "react-icons/im";
+import { GrHadoop } from "react-icons/gr";
 import { IoTimerOutline } from "react-icons/io5";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -141,11 +156,113 @@ function About() {
 					<Container>
 						<Grid container>
 							<Grid item xl={6} sm={12} md={6} lg={6}>
-								<div className="tools"></div>
-								<div className="tools"></div>
-								<div className="tools"></div>
+								<div className="abtPgSkill__">
+									<h6>Skills</h6>
+									<div className="abtPgSkillShow__">
+										<div className="abt1__">
+											<GiCheckMark className="abt1__Icon" />
+											<p>Data anaysis</p>
+										</div>
+										<div className="abt1__">
+											<GiCheckMark className="abt1__Icon" />
+											<p>
+												Data visualisation and
+												Communication
+											</p>
+										</div>
+										<div className="abt1__">
+											<GiCheckMark className="abt1__Icon" />
+											<p>
+												Data Science and
+												BusinessIntelligence
+											</p>
+										</div>
+										<div className="abt1__">
+											<GiCheckMark className="abt1__Icon" />
+											<p>Data engeneering</p>
+										</div>
+										<div className="abt1__">
+											<GiCheckMark className="abt1__Icon" />
+											<p>Machine Learning and AI</p>
+										</div>
+									</div>
+								</div>
 							</Grid>
-							<Grid item xl={6} sm={12} md={6} lg={6}></Grid>
+							<Grid item xl={6} sm={12} md={6} lg={6}>
+								<div className="tech-stack">
+									<div className="tech-stack-main">
+										<h6>Tech-Stack</h6>
+										<div className="abt1__">
+											<FaPython className="abt1__Icon" />
+											<p>Python</p>
+										</div>
+										<div className="abt1__">
+											<FaHtml5 className="abt1__Icon" />
+											<p>Html</p>
+										</div>
+										<div className="abt1__">
+											<IoLogoTableau className="abt1__Icon" />
+											<p>Tableau</p>
+										</div>
+
+										<div className="abt1__">
+											<SiPowerbi className="abt1__Icon" />
+											<p>PowerBi</p>
+										</div>
+
+										<div className="abt1__">
+											<SiApachespark className="abt1__Icon" />
+											<p>ApacheSpark</p>
+										</div>
+										<div className="abt1__">
+											<GrHadoop className="abt1__Icon" />
+											<p>Hadoop</p>
+										</div>
+										<div className="abt1__">
+											<SiFlask className="abt1__Icon" />
+											<p>Flask</p>
+										</div>
+									</div>
+									<div className="teck-stack-database">
+										<h6>Database and Cloud services</h6>
+										<div className="abt1__">
+											<SiPostgresql className="abt1__Icon" />
+											<p>PostgressSql</p>
+										</div>
+										<div className="abt1__">
+											<SiMicrosoftazure className="abt1__Icon" />
+											<p>Ms Azure</p>
+										</div>
+										<div className="abt1__">
+											<SiAmazonaws className="abt1__Icon" />
+											<p>AWS</p>
+										</div>
+										<div className="abt1__">
+											<SiMysql className="abt1__Icon" />
+											<p>MYSQL</p>
+										</div>
+									</div>
+									<div className="teck-stack-tools">
+										<h6>Tools and IDEs</h6>
+										<div className="abt1__">
+											<SiJupyter className="abt1__Icon" />
+											<p>Jupyter</p>
+										</div>
+										<div className="abt1__">
+											<SiVisualstudio className="abt1__Icon" />
+											<p>Visual Studio</p>
+										</div>
+										<div className="abt1__">
+											<SiHeroku className="abt1__Icon" />
+											<p>Heroku</p>
+										</div>
+										<div className="abt1__">
+											<ImGit className="abt1__Icon" />
+											<p>Git & Version Control</p>
+										</div>
+									</div>
+								</div>
+							</Grid>
 						</Grid>
 					</Container>
 				</div>
